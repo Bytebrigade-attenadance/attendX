36b859e3fa2acde3349db2d909052de56f3e0c3b
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-05-04 20:08:06 +0530

    routes initialised

--- server/app.js
+++ server/app.js
@@ -22,3 +22,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+
+import studentRouter from "./routes/studentController/routes.js";
+
+app.use("/v1/students/", studentRouter);
+
+export {app};
---
+++ server/routes/studentController/routes.js
@@ -0,0 +1,8 @@
+import { Router } from express;
+const studentRouter = Router();
+
+studentRouter.route("/login").post();
+export default studentRouter;
+
+
+
