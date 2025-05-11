1835fadd4c08492e4fd592a9a7b5cbe6331069d3
Kuljeet Singh <kuljeet@Kuljeets-MacBook-Air.local>
2025-05-11 20:31:07 +0530

    update

--- client/app.config.js
+++ client/app.config.js
@@ -3,6 +3,9 @@ export default({config})=>({
   ...config,
   extra:{
     apiUrl:process.env.API_BASE_URL,
-    router:{}
+    router:{},
+    "eas": {
+        "projectId": "da5b7df6-f3c8-483e-b787-e72f34dcc928"
+      },
   }
 })
